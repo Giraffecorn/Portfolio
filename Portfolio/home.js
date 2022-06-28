5bede9f7c60440e8f31ad9e62b0b3e6b4f61543f
Giraffecorn <107954481+Giraffecorn@users.noreply.github.com>
2022-06-27 20:01:58 -0400

    Navar

--- Portfolio/home.js
+++ Portfolio/home.js
@@ -1,3 +1,10 @@
-$(".Fire").hover(function() {
-    $("body").css("bsckground-color", "white");
+$(document).ready(function() {
+    $(".Fire").hover(function() {
+        $("body").css("background-image", "url('images/Fire.webp')");
+    }, function() {
+        $("body").css("background-image", "none");
+    });
+    $(".Menu").click(function() {
+        $(".Navbar").css("width","200px");
+    });
 });
