1ca2abdd7004f3bff17d4b0bc904ef0e7d6d1b97
Giraffecorn <107954481+Giraffecorn@users.noreply.github.com>
2022-06-29 18:40:38 -0400

    About Me

---
+++ Portfolio/content.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+//Navbar
+    $(".Menu").click(function() {
+        $(".Navbar").css("width","200px");
+    });
+//About Me Juqery
+    $(".key").click (function() {
+        $("iframe").css("visibility", "visible");
+    });
+});
