faf32fbf704e08238600bc4f574eeb55a494cb35
Giraffecorn <107954481+Giraffecorn@users.noreply.github.com>
2022-06-30 15:12:36 -0400

    Img

--- Portfolio/content.js
+++ Portfolio/content.js
@@ -3,8 +3,14 @@ $(document).ready(function() {
     $(".Menu").click(function() {
         $(".Navbar").css("width","200px");
     });
-//About Me Juqery
+//About Me Jquery
     $(".key").click (function() {
         $("iframe").css("visibility", "visible");
     });
 });
+$(document).ready(function() {
+    $(".Hard").click(function() {
+        alert ("This game is hard, if your score is positive you win");
+        alert ("The controls are Left, Right,and Up arrow keys");
+    });
+})
